e30bfb575026278ab9dbef6a1b7cd31063e1921c
Marco Antonio <48203561+MarcoAntonioCordero@users.noreply.github.com>
2020-04-24 16:09:25 -0500

    FISRT

---
+++ assets/js/tables.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+//$(".inicio").hide()
+
+
+$("#todos").click(function() {
+  $(".inicio").show()
+})
+
+$("#useraten").click(function() {
+  $(".inicio").hide()
+  $("#useratent").show()
+})
+$("#tipore").click(function() {
+  $(".inicio").hide()
+  $("#tiporet").show()
+})
+$("#cat").click(function() {
+  $(".inicio").hide()
+  $("#catt").show()
+})
+$("#subca").click(function() {
+  $(".inicio").hide()
+  $("#subcat").show()
+})
+$("#percep").click(function() {
+  $(".inicio").hide()
+  $("#percept").show()
+})
+$("#rutas").click(function() {
+  $(".inicio").hide()
+  $("#rutast").show()
+})
+$("#hour").click(function() {
+  $(".inicio").hide()
+  $("#hourt").show()
+})
+$("#edad").click(function() {
+  $(".inicio").hide()
+  $("#edadt").show()
+})
+$("#genero").click(function() {
+  $(".inicio").hide()
+  $("#generot").show()
+})
+
+})
